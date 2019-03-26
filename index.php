4ecb9454a09926fd91b9183cdea8093b91e574a8
Nir <nirbenami27@gmail.com>
2019-03-26 20:05:07 +0200

    First version 1.1

--- index.php
+++ index.php
@@ -7,7 +7,7 @@
     <script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.2.0/angular.min.js"></script>
     <script src="js/lba.js"></script>
 </head>
-<body id="allBody" style="text-align: center" ng-controller='ng-cases' ng-init="init_case();">
+<body ng-controller='ng-cases' ng-init="init_page();">
     
 	<?php
 		include "html/main.html";
