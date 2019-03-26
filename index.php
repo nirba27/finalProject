09dcf328bfa3592677f68e1b3236bee0abc6b134
Nir <nirbenami27@gmail.com>
2019-03-26 20:10:27 +0200

    First version 1.1

--- index.php
+++ index.php
@@ -6,6 +6,14 @@
 <head>
     <script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.2.0/angular.min.js"></script>
     <script src="js/lba.js"></script>
+    <!-- JQuery -->
+    <script type="text/javascript" src="js/jquery-3.3.1.min.js"></script>
+    <!-- Bootstrap tooltips -->
+    <script type="text/javascript" src="js/popper.min.js"></script>
+    <!-- Bootstrap core JavaScript -->
+    <script type="text/javascript" src="js/bootstrap.min.js"></script>
+    <!-- MDB core JavaScript -->
+    <script type="text/javascript" src="js/mdb.min.js"></script>
 </head>
 <body ng-controller='ng-cases' ng-init="init_page();">
     
