528bb29a62585b6d3fb130ba51d71d799f2ad244
Nir <nirbenami27@gmail.com>
2019-03-26 20:31:58 +0200

    First version 1.1

--- index.php
+++ index.php
@@ -1,26 +1,12 @@
-<html lang="en" ng-app="template" >
-<title>LBA</title>
-<head>
-    <script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.2.0/angular.min.js"></script>
-    <script src="js/lba.js"></script>
-    <!-- JQuery -->
-    <script type="text/javascript" src="js/jquery-3.3.1.min.js"></script>
-    <!-- Bootstrap tooltips -->
-    <script type="text/javascript" src="js/popper.min.js"></script>
-    <!-- Bootstrap core JavaScript -->
-    <script type="text/javascript" src="js/bootstrap.min.js"></script>
-    <!-- MDB core JavaScript -->
-    <script type="text/javascript" src="js/mdb.min.js"></script>
-    <!-- Initializations -->
-    <script type="text/javascript">
-        // Animations initialization
-        new WOW().init();
 
-    </script>
+
+
+<html lang="en" ng-app="template" >
+<head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <meta http-equiv="x-ua-compatible" content="ie=edge">
-    <title>Material Design Bootstrap</title>
+    <title>LBA</title>
     <!-- Font Awesome -->
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.0/css/all.css">
     <!-- Bootstrap core CSS -->
@@ -29,6 +15,8 @@
     <link href="css/mdb.min.css" rel="stylesheet">
     <!-- Your custom styles (optional) -->
     <link href="css/style.min.css" rel="stylesheet">
+    <script src="js/angular.min.js"></script>
+    <script src="js/lba.js"></script>
 
     <style type="text/css">
         html,
@@ -66,7 +54,8 @@
 
     </style>
 </head>
-<body ng-controller='ng-cases' ng-init="init_page();">
+</head>
+<body id="allBody" style="text-align: center" ng-controller='ng-cases' ng-init="init_case();">
     
 	<?php
 		include "html/main.html";
