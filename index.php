5e7827e5b2cdf6619a3e1aa9fb5f325f2c551756
Nir <nirbenami27@gmail.com>
2019-03-26 19:32:04 +0200

    First version 1.1

---
+++ index.php
@@ -0,0 +1,15 @@
+
+
+
+<html lang="en" ng-app="template" >
+<title>LBA</title>
+<head>
+    <script src="js/angular.min.js"></script>
+</head>
+<body id="allBody" style="text-align: center" ng-controller='ng-cases' ng-init="init_case();">
+    
+	<?php
+		include "html/main.html";
+	?>
+</body>
+</html>
