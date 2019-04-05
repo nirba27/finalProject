1153c642874e4c3c59248d1bb9d7c9eac8e75b57
Nir <nirbenami27@gmail.com>
2019-04-05 20:52:39 +0300

    First version 1.1

--- index.php
+++ index.php
@@ -54,8 +54,8 @@
         </div>
         <div class="call-to-action">
             <button id="login-button" type="button" ng-click="login();">Log In</button>
-            <center><p>Don't have an account? <br>
-                <a>Sign Up</a></p></center>
+            <p>Don't have an account? <br>
+                <a>Sign Up</a></p>
         </div>
     </div>
 </div>
