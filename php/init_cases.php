eaaaa2a4bfdedb9538a4933d5b586895dbfc512f
Nir <nirbenami27@gmail.com>
2019-05-06 21:15:25 +0300

    First version 1.1

--- php/init_cases.php
+++ php/init_cases.php
@@ -19,7 +19,7 @@ $connectionOptions = array(
 //Establishes the connection
 $conn = sqlsrv_connect($serverName, $connectionOptions);
 
-$tsql= "SELECT DISTINCT * FROM key_cluster";
+$tsql= "SELECT DISTINCT * FROM program";
 $getResults= sqlsrv_query($conn, $tsql);
 echo ("Reading data from table" . PHP_EOL);
 if ($getResults == FALSE)
@@ -28,8 +28,8 @@ if ($getResults == FALSE)
 $array = array();
 while ($row = sqlsrv_fetch_array($getResults, SQLSRV_FETCH_ASSOC)) {
     $array[] = array(
-        'key'=>$row['kid'],
-        'cluster'=>$row['cid']
+        'key'=>$row['id'],
+        'cluster'=>$row['pname']
 
     );
 }
