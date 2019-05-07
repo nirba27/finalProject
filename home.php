cf38f2ff10fa680342780ebacc9bf1db396d794a
Nir <nirbenami27@gmail.com>
2019-05-07 16:18:56 +0300

    version 1.2

--- home.php
+++ home.php
@@ -1070,14 +1070,10 @@
           <hr class="mb-5">
 
           <!--Carousel Wrapper-->
-          <div id="carousel-example-2" class="carousel slide carousel-fade" data-ride="carousel">
-              <!--Indicators-->
-              <ol ng-repeat="x in programs" class="carousel-indicators">
-                  <li data-target="#{{x.cover}}" data-slide-to="{{$index}}"></li>
-              </ol>
-              <!--/.Indicators-->
+          <div id="carousel-example-2" ng-repeat="x in programs"   class="carousel slide carousel-fade" data-ride="carousel">
+
               <!--Slides-->
-              <div  ng-repeat="x in programs"  class="carousel-inner" role="listbox">
+              <div  class="carousel-inner" role="listbox">
                   <div class="carousel-item">
                       <div class="view">
                           <img class="d-block w-100" src="{{x.cover}}"
@@ -1091,16 +1087,7 @@
                   </div>
               </div>
               <!--/.Slides-->
-              <!--Controls-->
-              <a class="carousel-control-prev" href="#carousel-example-2" role="button" data-slide="prev">
-                  <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                  <span class="sr-only">Previous</span>
-              </a>
-              <a class="carousel-control-next" href="#carousel-example-2" role="button" data-slide="next">
-                  <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                  <span class="sr-only">Next</span>
-              </a>
-              <!--/.Controls-->
+
           </div>
           <!--/.Carousel Wrapper-->
 
