50359744a8b7acfa1ab39ab7fccd20ebfb017b25
Nir <nirbenami27@gmail.com>
2019-05-07 16:16:29 +0300

    version 1.2

--- home.php
+++ home.php
@@ -1069,6 +1069,42 @@
           <!--Grid column-->
           <hr class="mb-5">
 
+          <!--Carousel Wrapper-->
+          <div id="carousel-example-2" class="carousel slide carousel-fade" data-ride="carousel">
+              <!--Indicators-->
+              <ol ng-repeat="x in programs" class="carousel-indicators">
+                  <li data-target="#{{x.cover}}" data-slide-to="{{$index}}"></li>
+              </ol>
+              <!--/.Indicators-->
+              <!--Slides-->
+              <div  ng-repeat="x in programs"  class="carousel-inner" role="listbox">
+                  <div class="carousel-item">
+                      <div class="view">
+                          <img class="d-block w-100" src="{{x.cover}}"
+                               alt="First slide">
+                          <div class="mask rgba-black-light"></div>
+                      </div>
+                      <div class="carousel-caption">
+                          <h3 class="h3-responsive">Light mask</h3>
+                          <p>First text</p>
+                      </div>
+                  </div>
+                  </div>
+              </div>
+              <!--/.Slides-->
+              <!--Controls-->
+              <a class="carousel-control-prev" href="#carousel-example-2" role="button" data-slide="prev">
+                  <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                  <span class="sr-only">Previous</span>
+              </a>
+              <a class="carousel-control-next" href="#carousel-example-2" role="button" data-slide="next">
+                  <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                  <span class="sr-only">Next</span>
+              </a>
+              <!--/.Controls-->
+          </div>
+          <!--/.Carousel Wrapper-->
+
           <img ng-repeat="x in programs" src="{{x.cover}}" class="wow fadeInUp" data-wow-delay="0.{{$index}}*2s">
       </section>
       <!--Section: Main features & Quick Start-->
