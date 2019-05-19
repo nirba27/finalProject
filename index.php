dbf526ddebdd37224fbe8aa06fcef5fb5fa83416
Nir <nirbenami27@gmail.com>
2019-05-19 18:59:55 +0300

    1.2

--- index.php
+++ index.php
@@ -63,7 +63,7 @@
         </div>
     </div>
 </div>
-<script src='http://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js'></script>
+<script src='https://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js'></script>
 <script src='https://cdn.jsdelivr.net/velocity/1.2.2/velocity.min.js'></script>
 <script src='https://cdn.jsdelivr.net/velocity/1.2.2/velocity.ui.min.js'></script>
 <script  src="login/js/index.js"></script>
