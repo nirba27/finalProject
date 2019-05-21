a977d04be46996fb2b5e5a6194048f92293fab45
Nir <nirbenami27@gmail.com>
2019-05-21 13:06:36 +0300

    1.3

--- home.php
+++ home.php
@@ -983,23 +983,19 @@
                     <div class="card">
                         <div class="card-body">
                             <h4 class="card-title">Cluster Number</h4>
-                            <p class="card-text">{{cluster}}</p><i class="fas fa-number"></i>
+                            <p class="card-text">{{cluster}}</p><i class="fas fa-sun"></i>
                         </div>
                     </div>
                     <div class="card">
                         <div class="card-body">
-                            <h4 class="card-title">Card title</h4>
-                            <p class="card-text">Some example text. Some example text.</p>
-                            <a href="#" class="card-link">Card link</a>
-                            <a href="#" class="card-link">Another link</a>
+                            <h4 class="card-title">Cluster Number</h4>
+                            <p class="card-text">{{cluster}}</p><i class="fas fa-sun"></i>
                         </div>
                     </div>
                     <div class="card">
                         <div class="card-body">
-                            <h4 class="card-title">Card title</h4>
-                            <p class="card-text">Some example text. Some example text.</p>
-                            <a href="#" class="card-link">Card link</a>
-                            <a href="#" class="card-link">Another link</a>
+                            <h4 class="card-title">Cluster Number</h4>
+                            <p class="card-text">{{cluster}}</p><i class="fas fa-sun"></i>
                         </div>
                     </div>
                 </div>
