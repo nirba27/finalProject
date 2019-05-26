0214c108427cd6eb1bddda01d759cf78a795423a
Nir <nirbenami27@gmail.com>
2019-05-26 23:16:21 +0300

    1.2

--- home.php
+++ home.php
@@ -1354,7 +1354,7 @@
     });
     function plotConceptMap()
     {
-        d3.json("metadata.json", function(dataJson) {
+        d3.json("metadata2.json", function(dataJson) {
             var plot = new ConceptMap("graph", "graph-info", dataJson);
         });
     }
