5760d2e868668dc386aa1d2fad954d0b657890fb
Nir <nirbenami27@gmail.com>
2019-05-30 14:11:47 +0300

    1.4

--- js/lba.js
+++ js/lba.js
@@ -189,7 +189,7 @@ app.controller('ng-cases', function ($scope, $http, $interval, fileUpload) {
     }
 
     $scope.get_audience2 = function () {
-
+        console.log("get_audience2");
         let search_str = '';
         console.log("GENDER:" + $scope.Gender);
         console.log("M" + $scope.maritial);
