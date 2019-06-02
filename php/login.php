a902453613684897e6374b5ebd69735d607673c1
Nir <nirbenami27@gmail.com>
2019-06-02 22:42:29 +0300

    1.9

--- php/login.php
+++ php/login.php
@@ -5,36 +5,38 @@
  * Date: 24/03/2019
  * Time: 20:28
  */
+// Always start this first
+session_start();
 
-ini_set('display_errors', 1);
-
-
-$serverName = "lba.database.windows.net"; // update me
-$connectionOptions = array(
-    "Database" => "lba", // update me
-    "Uid" => "nirbe", // update me
-    "PWD" => "Nn123456" // update me
-);
+if ( ! empty( $_POST ) ) {
+    if ( isset( $_POST['user'] ) && isset( $_POST['pass'] ) ) {
+        // Getting submitted user data from database
+        $serverName = "lba.database.windows.net"; // update me
+        $connectionOptions = array(
+            "Database" => "lba", // update me
+            "Uid" => "nirbe", // update me
+            "PWD" => "Nn123456" // update me
+        );
 //Establishes the connection
-$conn = sqlsrv_connect($serverName, $connectionOptions);
+        $conn = sqlsrv_connect($serverName, $connectionOptions);
 
 
-$user = (stripslashes($_POST['user']));
-$pass = (stripslashes($_POST['pass']));
+        $user = (stripslashes($_POST['user']));
+        $pass = (stripslashes($_POST['pass']));
 
-$tsql= "SELECT * FROM [dbo].[lba_users]  Where [name]='$user'";
-$getResults= sqlsrv_query($conn, $tsql);
+        $tsql = "SELECT * FROM [dbo].[lba_users]  Where [name]='$user'";
+        $getResults = sqlsrv_query($conn, $tsql);
 
-while ($row = sqlsrv_fetch_array($getResults, SQLSRV_FETCH_ASSOC)) {
-   if ($pass==$row['pass'])
-   {
-       echo 1;
-   }
-   else
-   {
-       echo 0;
-   }
+        while ($row = sqlsrv_fetch_array($getResults, SQLSRV_FETCH_ASSOC)) {
+            if ($pass == $row['pass']) {
+                $_SESSION['user_id'] = $user;
+                echo 1;
+            } else {
+                echo 0;
+            }
 
+        }
+
+    }
 }
-
 ?>
