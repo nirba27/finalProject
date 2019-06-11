9cae39f5f852d8d1136b606968ad0c1a75c2adbf
Nir <nirbenami27@gmail.com>
2019-06-12 00:30:45 +0300

    1.12

--- php/get_hours_mfi.php
+++ php/get_hours_mfi.php
@@ -20,12 +20,12 @@ $connectionOptions = array(
 $conn = sqlsrv_connect($serverName, $connectionOptions);
 $cluster = (stripslashes($_POST['cluster']));
 
-$tsql= "SELECT AVG(C4)*0.2 as '0',AVG(C5) as '1',AVG(C6) as '2',AVG(C7) as '3',
+$tsql= "SELECT AVG(C27) as '23',AVG(C4)*0.2 as '0',AVG(C5) as '1',AVG(C6) as '2',AVG(C7) as '3',
 AVG(C8) as '4',AVG(C9) as '5',AVG(C10) as '6',AVG(C11) as '7',
 AVG(C12) as '8',AVG(C13) as '9',AVG(C14) as '10',AVG(C15) as '11',
 AVG(C16) as '12',AVG(C17) as '13',AVG(C18) as '14',AVG(C19) as '15',
 AVG(C20) as '16',AVG(C21) as '17',AVG(C22) as '18',AVG(C23) as '19',
-AVG(C24) as '20',AVG(C25) as '21',AVG(C26) as '22',AVG(C27) as '23'
+AVG(C24) as '20',AVG(C25) as '21',AVG(C26) as '22'
 FROM bar A1
 LEFT JOIN final_hist A2 ON A1.hhID=A2.hhID
 WHERE $cluster";
