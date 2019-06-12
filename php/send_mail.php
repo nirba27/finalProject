dcc4403c3416500c4577348972ac5cfa723c0a49
Nir <nirbenami27@gmail.com>
2019-06-12 17:38:45 +0300

    1.18

--- php/send_mail.php
+++ php/send_mail.php
@@ -3,7 +3,7 @@
 echo 'bla';
 //require 'vendor/autoload.php'; // If you're using Composer (recommended)
 // Comment out the above line if not using Composer
-require("sendgrid/sendgrid-php.php");
+require("../sendgrid/sendgrid-php.php");
 // If not using Composer, uncomment the above line and
 // download sendgrid-php.zip from the latest release here,
 // replacing <PATH TO> with the path to the sendgrid-php.php file,
